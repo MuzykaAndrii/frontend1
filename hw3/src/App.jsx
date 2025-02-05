8fe43fd5529ccdea4d28e47449ac843817f4f2fa
MuzykaAndrii <andrymyzik@gmail.com>
2025-02-05 12:59:10 +0200

    impl: dynamic buttons in header appropriate to auth state

--- hw3/src/App.jsx
+++ hw3/src/App.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react"
 import HeaderComponent from "./common/components/Header"
 
 
 export default function AppComponent() {
+    let [isAuthenticated, setAuth] = useState(false);
+
     return <>
-        <HeaderComponent />
+        <HeaderComponent isAuthenticated={isAuthenticated} />
         <div className='container'></div>
     </>
 };
