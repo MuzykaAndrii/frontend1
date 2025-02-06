c2a803c721fd440f236b8dafd57e7824b3919410
MuzykaAndrii <andrymyzik@gmail.com>
2025-02-06 16:04:07 +0200

    impl: template for current chat

--- hw3/src/chats/components/Chat.jsx
+++ hw3/src/chats/components/Chat.jsx
@@ -20,10 +20,9 @@ export default function ChatComponent() {
     }, [client]);
 
     return <>
-        <div className="row">
+        <div className="row my-5" style={{ height: "80vh" }}>
             <div className="col-md-5 chatList">
                 <div className="list-group">
-
                     {chats.map((chat) => (
                         <a href="#" key={chat.id} className="list-group-item list-group-item-action">
                             <div className="d-flex w-100 justify-content-between">
@@ -34,11 +33,21 @@ export default function ChatComponent() {
                             <small className="text-body-secondary">Last message</small>
                         </a>
                     ))}
-
                 </div>
             </div>
-            <div className="col-md-7 currentChat">
 
+            <div className="col-md-7 currentChat d-flex flex-column">
+                <div className="chatHistory flex-grow-1 overflow-auto p-3">
+                </div>
+
+                <div className="row g-3 sendMsgArea p-3">
+                    <div className="col-11">
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
+                    </div>
+                    <div className="col-1">
+                        <button type="submit" className="btn btn-primary mb-3">Send</button>
+                    </div>
+                </div>
             </div>
         </div>
 
